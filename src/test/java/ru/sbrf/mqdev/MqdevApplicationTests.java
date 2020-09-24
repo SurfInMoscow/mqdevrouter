fb480697e5b52b097bb54933fd2441127c64a971
Pavel Vorobev <p.vorobyev@inbox.ru>
2020-09-24 15:33:35 +0300

    git init

---
+++ src/test/java/ru/sbrf/mqdev/MqdevApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.sbrf.mqdev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MqdevApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
